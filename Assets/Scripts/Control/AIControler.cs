8efb6d11b3e5ac234f4632d11594f69c11284bf2
Mike Walters <mike@beringbullet.com>
2021-07-24 14:39:36 -0500

    suspiction time

--- Assets/Scripts/Control/AIControler.cs
+++ Assets/Scripts/Control/AIControler.cs
@@ -2,43 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 namespace RPG.Control
 {
     public class AIControler : MonoBehaviour
     {
-        [SerializeField] float ChaseDistance = 5f;
+        [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 5f;
         GameObject player;
         Fighter fighter;
         Health health;
+        Mover mover;
+        ActionScheduler actionScheduler;
+        Vector3 guardLocatoin;
+        Quaternion guardDefaultRotation;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
         private void Start()
         {
             player = GameObject.FindWithTag("Player");
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
+            actionScheduler = GetComponent<ActionScheduler>();
+
+            guardLocatoin = transform.position;
+            guardDefaultRotation = transform.rotation;
         }
         private void Update()
         {
             if (health.isDead) return;
             if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                timeSinceLastSawPlayer = 0;
+                AttackBehaviour();
+            }
+            else if (timeSinceLastSawPlayer < suspicionTime)
+            {
+                SuspicionBehaviour();
             }
             else
             {
-                fighter.Cancel();
+                GuardBehaviour();
             }
+            timeSinceLastSawPlayer += Time.deltaTime;
         }
+
+        private void GuardBehaviour()
+        {
+            mover.StartMoveAction(guardLocatoin);
+        }
+
+        private void SuspicionBehaviour()
+        {
+            actionScheduler.CancelCurrentAction();
+        }
+
+        private void AttackBehaviour()
+        {
+            fighter.Attack(player);
+        }
+
         private bool InAttackRangeOfPlayer()
         {
             float distance = Vector3.Distance(player.transform.position, transform.position);
-            return distance < ChaseDistance;
+            return distance < chaseDistance;
         }
 
         //called by Unity Editpr 
-        private void OnDrawGizmosSelected() {
+        private void OnDrawGizmosSelected()
+        {
             Gizmos.color = Color.blue;
-            Gizmos.DrawWireSphere(transform.position, ChaseDistance);
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
         }
     }
 }
