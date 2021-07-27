438493f4b9cc41e24bf4ca55918b284b5eff1e23
Mike Walters <mike@beringbullet.com>
2021-07-27 14:37:40 -0500

    breaking commit,  moving computers

---
+++ Assets/Scripts/Combat/Projectile.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Projectile : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
+
