3603245c98369fd4aecdc91f5fb26134d3e823ed
Mike Walters <mike@beringbullet.com>
2021-07-30 07:55:56 -0500

    stats

---
+++ Assets/Scripts/Stats/Pregression.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    [CreateAssetMenu(fileName = "Pregression", menuName = "RPG-Bering/Pregression", order = 0)]
+    public class Pregression : ScriptableObject
+    {
+
+    }
+}
