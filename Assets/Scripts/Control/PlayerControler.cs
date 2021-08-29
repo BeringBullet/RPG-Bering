67fa63b4efdfbb01c2a18c4ec84807245c5c019b
Mike Walters <mike@beringbullet.com>
2021-08-29 18:32:56 -0500

    Alot of stuff

--- Assets/Scripts/Control/PlayerControler.cs
+++ Assets/Scripts/Control/PlayerControler.cs
@@ -3,23 +3,49 @@ using RPG.Movement;
 using System;
 using RPG.Combat;
 using RPG.Resources;
-
+using UnityEngine.EventSystems;
 
 namespace RPG.Control
 {
     public class PlayerControler : MonoBehaviour
     {
         Health health;
+        enum CursorType
+        {
+            None,
+            Movement,
+            Combat
+        }
 
+        [Serializable]
+        struct CursorMapping
+        {
+            public CursorType type;
+            public Texture2D texture;
+            public Vector2 hotspot;
+        }
+
+        [SerializeField] CursorMapping[] cursorMappings;
         private void Awake()
         {
             health = GetComponent<Health>();
         }
         void Update()
         {
-            if (health.isDead) return;
+            if (InteractWithIU()) return;
+            if (health.isDead) 
+            {
+                SetCursor(CursorType.None);
+                return;
+            }
             if (InteractWithCombat()) return;
             if (InteractWithMovement()) return;
+            SetCursor(CursorType.None);
+        }
+
+        private bool InteractWithIU()
+        {
+           return EventSystem.current.IsPointerOverGameObject();
         }
 
         private bool InteractWithCombat()
@@ -35,6 +61,7 @@ namespace RPG.Control
                 {
                     GetComponent<Fighter>().Attack(target.gameObject);
                 }
+                SetCursor(CursorType.Combat);
                 return true;
             }
             return false;
@@ -50,11 +77,25 @@ namespace RPG.Control
                 {
                     GetComponent<Mover>().StartMoveAction(hit.point, 1f);
                 }
+                SetCursor(CursorType.Movement);
                 return true;
             }
             return false;
         }
-
+        private void SetCursor(CursorType type)
+        {
+            CursorMapping mapping = GetCursorMapping(type);
+            Cursor.SetCursor(mapping.texture, mapping.hotspot, CursorMode.Auto);
+        }
+        private CursorMapping GetCursorMapping(CursorType type)
+        {
+            foreach (CursorMapping mapping in cursorMappings)
+            {
+                if (mapping.type == type)
+                    return mapping;
+            }
+            return cursorMappings[0];
+        }
         private static Ray GetMouseRay()
         {
             return Camera.main.ScreenPointToRay(Input.mousePosition);
